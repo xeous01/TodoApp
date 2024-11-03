ce7ea2f77585a80723aa8659f188cf1140777ffd
xeous01 <xeous01@gmail.com>
2024-11-03 17:15:38 +0900

    완성함 11/03

---
+++ TodoApp/TodoApp.ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using TodoApp.Models;
+
+namespace TodoApp.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ITodoRepository _repository = new TodoRepositoryInMemory();
+            List<Todo> todos = new List<Todo>();
+            todos = _repository.GetAll();
+            // 기본 데이터 출력
+            foreach (var t in todos)
+            {
+                Console.WriteLine($"{t.Id} - {t.Title}({t.IsDone})");
+            }
+            Console.WriteLine();
+
+            //[2] 데이터 입력
+            Todo todo = new Todo() { Title = "Database 학습", IsDone = true };
+            _repository.Add(todo);
+            
+            
+            //[3] 추가 데이터 출력
+
+            todos = _repository.GetAll();  // 다시로드
+            foreach (var t in todos)
+            {
+                Console.WriteLine($"{t.Id} - {t.Title}({t.IsDone})");
+            }
+
+        }
+    }
+}
+
---
+++ TodoApp/TodoApp.Models/TodoRepositoryInMemory.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TodoApp.Models
+{
+    public class TodoRepositoryInMemory : ITodoRepository
+    {
+        private static List<Todo> _tododos = new List<Todo>();
+
+        public TodoRepositoryInMemory()
+        {
+            _tododos = new List<Todo>
+               {
+
+                   new Todo {Id = 1, Title = "ASP.NET Core 학습" , IsDone = false},
+                   new Todo {Id = 2, Title = "Blazoe 학습" , IsDone = false},
+                   new Todo {Id = 3, Title = "C# 학습" , IsDone = true}
+
+               };
+        }
+        // 인 메모리 데이터 베이스 사용영역
+        public void Add(Todo model)
+        {
+            model.Id = _tododos.Max(t => t.Id) + 1;
+            _tododos.Add(model);
+        }
+
+        public List<Todo> GetAll()
+        {
+            return _tododos.ToList();
+        }
+    }
+
+
+}
